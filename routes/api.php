8e5368d029ab5520e75d12af5759a58d7560f1b8
Gustavo Freitas <gustafsilva.dev@gmail.com>
2024-06-13 09:56:47 -0300

    feat: add auth routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Api\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,5 +7,3 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
 
-Route::post('register', [AuthenticationController::class, 'register'])->name('register');
-Route::post('login', [AuthenticationController::class, 'login'])->name('login');
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,7 @@
 <?php
+use App\Http\Controllers\SocialLoginController;
+use Illuminate\Support\Facades\Route;
 
 
+Route::get('auth/{provider}/redirect', [SocialLoginController::class , 'redirect'])->name('redirect');
+Route::get('auth/{provider}/callback', [SocialLoginController::class , 'callback'])->name('callback');
